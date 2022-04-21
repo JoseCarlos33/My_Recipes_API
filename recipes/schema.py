c2e1f43a9bb15da70594b0457fc633932a159d27
José Carlos Noronha <joseph.finder33@gmail.com>
2022-04-21 18:48:00 -0300

    :art: Adjust Old Querys

--- recipes/schema.py
+++ recipes/schema.py
@@ -3,8 +3,8 @@ from atexit import register
 import graphene
 from graphene_django import DjangoObjectType
 from . import models
-from graphql_auth import mutations
-from graphql_auth.schema import UserQuery
+from graphql_auth import mutations 
+from graphql_auth.schema import UserQuery, MeQuery
 
 class UserProfileType(DjangoObjectType):
     class Meta:
@@ -25,7 +25,6 @@ class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
     verify_account = mutations.VerifyAccount.Field()
     token_auth = mutations.ObtainJSONWebToken.Field()
-    update_account = mutations.UpdateAccount.Field()
     resend_activation_email = mutations.ResendActivationEmail.Field()
     send_password_reset_email = mutations.SendPasswordResetEmail.Field()
     password_reset = mutations.PasswordReset.Field()
@@ -33,18 +32,7 @@ class AuthMutation(graphene.ObjectType):
 class Query(UserQuery, graphene.ObjectType):
     all_recipes = graphene.List(RecipeType)
     public_recipes = graphene.List(RecipeType)
-    my_profile = graphene.Field(UserProfileType)
-
-    def resolve_my_profile(root, info):
-        user = info.context.user
-        if not user.is_active:
-            raise Exception('Not logged in!')
-
-        return models.UserProfile.objects.get(pk=user.pk)
-
-
-    def resolve_all_users(root, info):
-        return models.UserProfile.objects.all()
+    my_profile = graphene.Field(UserProfileType, id=graphene.String(required=True))
 
     def resolve_public_recipes(root, info):
         return models.Recipe.objects.filter(public=True)
@@ -53,7 +41,7 @@ class Query(UserQuery, graphene.ObjectType):
         return models.Recipe.objects.all()
     
 
-class Mutation(AuthMutation,graphene.ObjectType):
+class Mutation(AuthMutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
